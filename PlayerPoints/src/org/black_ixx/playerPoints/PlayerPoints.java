01da06347a24fd51f334fc4ece0e813b02042a6e
Vincent Deloso <mitsugaru@gmail.com>
2012-08-19 17:38:41 -0600

    Added softdepend on Votifier.

--- PlayerPoints/src/org/black_ixx/playerPoints/PlayerPoints.java
+++ PlayerPoints/src/org/black_ixx/playerPoints/PlayerPoints.java
@@ -9,47 +9,45 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PlayerPoints extends JavaPlugin {
-    public static final String TAG = "[PlayerPoints]";
-    private RootConfig rootConfig;
-    private StorageHandler storage;
+public class PlayerPoints extends JavaPlugin{
+   public static final String TAG = "[PlayerPoints]";
+   private RootConfig rootConfig;
+   private StorageHandler storage;
 
-    @Override
-    public void onEnable() {
-	// Initialize localization
-	LocalizeConfig.init(this);
-	// Initialize config
-	rootConfig = new RootConfig(this);
-	// Intialize storage handler
-	storage = new StorageHandler(this);
-	// Initialize API
-	PlayerPointsAPI.init(this);
-	// Initialize updater
-	Update.init(this);
-	Update.checkUpdate();
-	// Register commands
-	final Commander commander = new Commander();
-	getCommand("points").setExecutor(commander);
-	getCommand("p").setExecutor(commander);
-	// Register votifier listener, if applicable
-	if (rootConfig.voteEnabled) {
-	    final PluginManager pm = getServer().getPluginManager();
-	    final Plugin votifier = pm.getPlugin("Votifier");
-	    if (votifier != null) {
-		pm.registerEvents(new VotifierListener(this), this);
-	    }
-	    else
-	    {
-		getLogger().warning("Could not hook into Votifier!");
-	    }
-	}
-    }
+   @Override
+   public void onEnable(){
+      // Initialize localization
+      LocalizeConfig.init(this);
+      // Initialize config
+      rootConfig = new RootConfig(this);
+      // Intialize storage handler
+      storage = new StorageHandler(this);
+      // Initialize API
+      PlayerPointsAPI.init(this);
+      // Initialize updater
+      Update.init(this);
+      Update.checkUpdate();
+      // Register commands
+      final Commander commander = new Commander();
+      getCommand("points").setExecutor(commander);
+      getCommand("p").setExecutor(commander);
+      // Register votifier listener, if applicable
+      if(rootConfig.voteEnabled){
+         final PluginManager pm = getServer().getPluginManager();
+         final Plugin votifier = pm.getPlugin("Votifier");
+         if(votifier != null){
+            pm.registerEvents(new VotifierListener(this), this);
+         }else{
+            getLogger().warning("Could not hook into Votifier!");
+         }
+      }
+   }
 
-    public RootConfig getRootConfig() {
-	return rootConfig;
-    }
+   public RootConfig getRootConfig(){
+      return rootConfig;
+   }
 
-    public StorageHandler getStorageHandler() {
-	return storage;
-    }
+   public StorageHandler getStorageHandler(){
+      return storage;
+   }
 }
